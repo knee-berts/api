ef362afa96d9c9eb9f6d83e6a0aca6e3b251d077
Nicholas Eberts <nicholas.eberts@yahoo.com>
2018-04-13 22:30:35 -0400

    fixed uri

--- app.js
+++ app.js
@@ -8,20 +8,22 @@ var bodyParser = require("body-parser");
 var async = require("async");
 const mongoose = require("mongoose");
 
-var MONGODB_HOST = process.env.MONGODB_HOST;
-console.log(`HOST: `, MONGODB_HOST);
-var MONGODB_PORT = process.env.MONGODB_PORT;
-console.log(`PORT: `, MONGODB_PORT);
-var MONGODB_USERNAME = process.env.MONGODB_USERNAME;
-console.log(`USERNAME: `, MONGODB_USERNAME);
-var MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
-console.log(`PASSWORD: `, MONGODB_PASSWORD);
-var MONGODB_DBNAME = process.env.MONGODB_DBNAME;
-console.log(`DBNAME: `, MONGODB_DBNAME);
-var MONGODB_URI = process.env.MONGODB_URI;
+// var MONGODB_HOST = process.env.MONGODB_HOST;
+// console.log(`HOST: `, MONGODB_HOST);
+// var MONGODB_PORT = process.env.MONGODB_PORT;
+// console.log(`PORT: `, MONGODB_PORT);
+// var MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+// console.log(`USERNAME: `, MONGODB_USERNAME);
+// var MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+// console.log(`PASSWORD: `, MONGODB_PASSWORD);
+// var MONGODB_DBNAME = process.env.MONGODB_DBNAME;
+// console.log(`DBNAME: `, MONGODB_DBNAME);
 
-var proto = "mongodb://";
-var URI = proto.concat(MONGODB_USERNAME, ":", MONGODB_PASSWORD, "@", MONGODB_HOST,":", MONGODB_PORT, "/", MONGODB_DBNAME, "?ssl=true");
+var URI = process.env.MONGODB_URI;
+console.log(`URI: `, URI);
+
+// var proto = "mongodb://";
+// var URI = proto.concat(MONGODB_USERNAME, ":", MONGODB_PASSWORD, "@", MONGODB_HOST,":", MONGODB_PORT, "/", MONGODB_DBNAME, "?ssl=true");
 
 console.log(`about to connect to DB with connection string `, URI);
 
@@ -85,7 +87,7 @@ const reconnectTimeout = 10000; // ms.
 // }
 
 function connect() {
-  mongoose.connect(process.env.MONGODB_URI, connectOptions).catch(() => {});
+  mongoose.connect(URI, connectOptions).catch(() => {});
 }
 
 // make sure your connected
